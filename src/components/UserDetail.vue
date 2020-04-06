a03056317cb8c3af1390dcb9a70dcf49e6482fd3
Maheeb <maheeb009@gmail.com>
2020-04-07 01:47:09 +0600

    props

---
+++ src/components/UserDetail.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User name: {{switchName()}}</p>
+        <button @click="resetName">Reset Name</button>
+    </div>
+</template>
+
+<script>
+    export default {
+
+            props: ['name'],
+
+           methods:{
+                switchName: function () {
+
+                    return this.name.split("").reverse().join("");
+                },
+
+               resetName: function () {
+                   this.name="Maheeb";
+                   this.$emit('namewasReset', this.name);
+               }
+
+
+           }
+
+
+
+
+    }
+
+
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
