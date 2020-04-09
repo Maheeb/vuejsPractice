c635d0da9b4ba9d980c46719c8db42f5e11b2626
Maheeb <maheeb009@gmail.com>
2020-04-09 21:15:46 +0600

    fade and slide

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,96 @@
 <template>
   <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <h3>Animations</h3>
+          <hr>
+          <button class="btn btn-primary" @click=" (show = !show) ">Button</button>
+        <br><br>
+            <transition name="fade">
+              <div class="alert alert-info" v-if="show" >This is alert</div>
+            </transition>
+          <transition name="slide">
+              <div class="alert alert-info" v-if="show" >This is alert</div>
+          </transition>
 
-        <input type="text" v-model="filteredText">
-        <ul>
-            <li v-for="(item,index) in filteredFruits" :index="index" :item="item" :key="index.id">
+      </div>
+    </div>
 
-              {{item}}
-
-            </li>
-        </ul>
-        <hr>
-        <app-list></app-list>
 
   </div>
 </template>
 
 <script>
-  import {fruitMixin} from "./fruitMixin";
-  import List from './components/List'
+
     export default {
-      mixins: [fruitMixin],
-        components:{
-            appList: List
-
-        },
-        data: function () {
+        data:function () {
             return{
-                text: 'Hello world',
-
+                show: false
             }
+        }
 
-        },
-
-        filters:{
-            toUppercase : function (value) {
-                return value.toUpperCase();
-
-            }
-        },
 
 
     }
 </script>
 
 <style>
+        .fade-enter{
+            opacity: 0;
+
+        }
+    .fade-enter-active{
+            transition: opacity 1s;
+            opacity: 1;
+    }
+    .fade-leave{
+
+
+    }
+    .fade-leave-active{
+        transition: opacity 1s;
+        opacity: 0;
+    }
+
+        .slide-enter{
+
+
+        }
+        .slide-enter-active{
+            animation: slide-in 1s ease-out forwards;
+
+        }
+        .slide-leave{
+
+
+        }
+        .slide-leave-active{
+            animation: slide-out 1s ease-out forwards;
+        }
+
+    @keyframes slide-in {
+
+        from{
+            transform: translateY(20px);
+        }
+        to{
+
+            transform: translateY(0);
+        }
+
+    }
+        @keyframes slide-out {
+
+            from{
+                transform: translateY(0);
+            }
+            to{
+
+                transform: translateY(20px);
+            }
+
+        }
+
+
 
 </style>
