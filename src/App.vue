0c4f39b76261cd4339c6a2b79181c7999156aefa
Maheeb <maheeb009@gmail.com>
2020-04-09 22:21:27 +0600

    Simple Quiz app

--- src/App.vue
+++ src/App.vue
@@ -1,96 +1,80 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-          <h3>Animations</h3>
-          <hr>
-          <button class="btn btn-primary" @click=" (show = !show) ">Button</button>
-        <br><br>
-            <transition name="fade">
-              <div class="alert alert-info" v-if="show" >This is alert</div>
-            </transition>
-          <transition name="slide">
-              <div class="alert alert-info" v-if="show" >This is alert</div>
-          </transition>
-
-      </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1 class="text-center">The Super Quiz</h1>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <transition name="flip" mode="out-in">
+                    <component :is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'"></component>
+                </transition>
+            </div>
+        </div>
     </div>
-
-
-  </div>
 </template>
 
 <script>
+    import Question from './components/Question.vue';
+    import Answer from './components/Answer.vue';
 
     export default {
-        data:function () {
-            return{
-                show: false
+        data() {
+            return {
+                mode: 'app-question'
             }
+        },
+        methods: {
+            answered(isCorrect) {
+                if (isCorrect) {
+                    this.mode = 'app-answer';
+                } else {
+                    this.mode = 'app-question';
+                    alert('Wrong, try again!');
+                }
+            }
+        },
+        components: {
+            appQuestion: Question,
+            appAnswer: Answer
         }
-
-
-
     }
 </script>
 
 <style>
-        .fade-enter{
-            opacity: 0;
-
-        }
-    .fade-enter-active{
-            transition: opacity 1s;
-            opacity: 1;
-    }
-    .fade-leave{
-
-
-    }
-    .fade-leave-active{
-        transition: opacity 1s;
-        opacity: 0;
+    .flip-enter {
+        /*transform: rotateY(0deg);*/
     }
 
-        .slide-enter{
-
-
-        }
-        .slide-enter-active{
-            animation: slide-in 1s ease-out forwards;
-
-        }
-        .slide-leave{
-
-
-        }
-        .slide-leave-active{
-            animation: slide-out 1s ease-out forwards;
-        }
-
-    @keyframes slide-in {
-
-        from{
-            transform: translateY(20px);
-        }
-        to{
-
-            transform: translateY(0);
-        }
-
+    .flip-enter-active {
+        animation: flip-in  0.5s ease-out forwards;
     }
-        @keyframes slide-out {
 
-            from{
-                transform: translateY(0);
-            }
-            to{
+    .flip-leave {
+        /*transform: rotateY(0deg);*/
+    }
 
-                transform: translateY(20px);
-            }
+    .flip-leave-active {
+        animation: flip-out 0.5s ease-out forwards;
+    }
 
+    @keyframes flip-out {
+        from {
+            transform: rotateY(0deg);
         }
+        to {
+            transform: rotateY(90deg);
+        }
+    }
 
-
-
+    @keyframes flip-in {
+        from {
+            transform: rotateY(90deg);
+        }
+        to {
+            transform: rotateY(0deg);
+        }
+    }
 </style>
