05e9c584ee2f0aafed22468ed2d5cb8236c14ec5
Maheeb <maheeb009@gmail.com>
2020-04-11 02:43:14 +0600

    Http request - Post and Get

--- src/App.vue
+++ src/App.vue
@@ -2,79 +2,95 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1 class="text-center">The Super Quiz</h1>
-            </div>
-        </div>
-        <hr>
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <transition name="flip" mode="out-in">
-                    <component :is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'"></component>
-                </transition>
+                <h1>Http</h1>
+                <div class="form-group">
+                    <label for="UserName">User Name</label>
+                    <input type="text" class="form-control" v-model="user.userName">
+                </div>
+                <div class="form-group">
+                    <label for="Mail">Email</label>
+                    <input type="text" class="form-control" v-model="user.email">
+                </div>
+
+                <button class="btn btn-primary"  @click="submit" >Submit</button>
+                <hr>
+                <button class="btn btn-primary" @click="fetchData">Get data</button>
+                <br><br>
+                <ul>
+                    <li class="list-group-item" v-for="(item, index) in users" :key="index" :item="item">
+                            {{item.userName}} - {{item.email}}
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import Question from './components/Question.vue';
-    import Answer from './components/Answer.vue';
-
     export default {
-        data() {
-            return {
-                mode: 'app-question'
-            }
+
+        data:function () {
+            return{
+                user: {
+                    userName: '',
+                    email: ''
+                },
+                users:[],
+                resource:{}
+            };
+
         },
-        methods: {
-            answered(isCorrect) {
-                if (isCorrect) {
-                    this.mode = 'app-answer';
-                } else {
-                    this.mode = 'app-question';
-                    alert('Wrong, try again!');
-                }
+        methods:{
+            submit:function () {
+
+                // this.$http.post('',this.user)
+                //     .then(response=>{
+                //        console.log(response)
+                //     },
+                //     error=>{
+                //         console.log(error)
+                //     }
+                //
+                //     );
+
+                this.resource.saveAlt(this.user);
+            },
+            fetchData: function () {
+
+                this.$http.get('', this.user)
+                    .then(response => {
+                        return response.json()
+                    })
+                    .then(data => {
+
+                        const resultArray = [];
+
+                        for (let key in data) {
+
+                            resultArray.push(data[key]);
+                        }
+                        this.users = resultArray;
+
+                    });
+
             }
-        },
-        components: {
-            appQuestion: Question,
-            appAnswer: Answer
-        }
+            },
+
+            created(){
+                const customActions ={
+
+                    saveAlt: {method: 'POST', url: 'alternative.json'}
+                };
+
+                this.resource = this.$resource('data.json',{}, customActions);
+
+            }
+
+
+
     }
 </script>
 
 <style>
-    .flip-enter {
-        /*transform: rotateY(0deg);*/
-    }
 
-    .flip-enter-active {
-        animation: flip-in  0.5s ease-out forwards;
-    }
-
-    .flip-leave {
-        /*transform: rotateY(0deg);*/
-    }
-
-    .flip-leave-active {
-        animation: flip-out 0.5s ease-out forwards;
-    }
-
-    @keyframes flip-out {
-        from {
-            transform: rotateY(0deg);
-        }
-        to {
-            transform: rotateY(90deg);
-        }
-    }
-
-    @keyframes flip-in {
-        from {
-            transform: rotateY(90deg);
-        }
-        to {
-            transform: rotateY(0deg);
-        }
-    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource';
 
-Vue.filter('to-lowercase', function (value) {
-    return value.toLowerCase();
-});
+Vue.use(VueResource)
+
+Vue.http.options.root='https://vuejs-azmaeen.firebaseio.com/';
 
 new Vue({
     el: '#app',
